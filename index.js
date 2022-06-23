f482818d40687f06e5ad2a142b0fb96d4611c82c
run2go <qkrwjdtn09@gmail.com>
2022-06-23 18:26:18 +0900

    ejs

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 var express = require("express");
 var app = express();
 
+app.set("view engine", "ejs"); // 1
 app.use(express.static(__dirname + "/public"));
+
+app.get("/hello", function (req, res) {
+  // 2
+  res.render("hello", { name: req.query.nameQuery });
+});
+
+app.get("/hello/:nameParam", function (req, res) {
+  // 3
+  res.render("hello", { name: req.params.nameParam });
+});
 
 var port = 3000;
 app.listen(port, function () {
